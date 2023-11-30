26c8a313604fa635d9d746a4e7a9037fe1fe2b1d
Medzic <aderizal10@gmail.com>
2023-11-30 12:08:26 +0700

    add dynamic port

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require("express");
 const app = express();
 const authRoute = require('./routes/auth')
 const indexRoute = require('./routes/index')
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(authRoute, indexRoute)
 
 app.listen(port, (req, res) => {
     console.log(`listen to port: ${port}`);
 });
+
+process.on('unhandledRejection', (err) => {
+    console.log(err);
+    process.exit();
+})
