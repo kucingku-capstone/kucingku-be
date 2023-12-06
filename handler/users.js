26e37e10626fe3d08dc311c2e1b061fa3fa0a678
Medzic <aderizal10@gmail.com>
2023-12-06 12:20:33 +0700

    adding update and delete for user profile

--- handler/users.js
+++ handler/users.js
@@ -1,17 +1,15 @@
 const con = require("../utils/db");
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-require("dotenv").config();
 
 const saltRounds = 10;
-const secretKey = process.env.KUCINGKU_JWT_SECRET;
 
-//utuk memvalidasi email apakah valid
+//validasi email
 const emailIsValid = (email) => {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailRegex.test(email);
 };
 
+//validasi email duplicat
 const isEmailRegistered = async (email) => {
   return new Promise((resolve, reject) => {
     con.query(
@@ -29,22 +27,16 @@ const isEmailRegistered = async (email) => {
   });
 };
 
-const registerHandler = async (req, res) => {
+const updateUserHandler = async (req, res) => {
   try {
-    const { username, email, password } = req.body;
-
+    const userId = req.params.id;
+    const { username, email, password, telp, alamat, roles } = req.body;
 
     if (!emailIsValid(email)) {
       res.status(400).json({ error: "Invalid Email Format" });
       return;
     }
 
-    if (!email || !password) {
-      res.status(400).json({ error: "Email and Password required" });
-      return;
-    }
-
-    // Check if the email is already registered
     const emailAlreadyRegistered = await isEmailRegistered(email);
     if (emailAlreadyRegistered) {
       res.status(400).json({ error: "Email already registered" });
@@ -55,8 +47,8 @@ const registerHandler = async (req, res) => {
 
     await new Promise((resolve, reject) => {
       con.query(
-        "INSERT INTO users (username, email, password) VALUES (?, ?, ?)",
-        [username, email, hashedPassword],
+        "UPDATE users SET username = ?, email = ?, password = ?, telp = ?, alamat = ?, roles = ? WHERE id = ?",
+        [username, email, hashedPassword, telp, alamat, roles, userId],
         (error, results, fields) => {
           if (error) {
             console.error("Database error:", error);
@@ -67,68 +59,39 @@ const registerHandler = async (req, res) => {
         }
       );
     });
-
-    res.status(200).json({ message: "User registered successfully" });
+    res.status(200).json({ message: "User profile updated successfully" });
   } catch (error) {
     console.error("Handler error:", error);
     res.status(error.code || 500).json({ error: "Internal Server Error" });
   }
 };
 
-//handler login
-const loginHandler = async (req, res) => {
-  const { email, password } = req.body;
 
-  if (!emailIsValid(email)) {
-    res.status(400).json({ error: "Invalid Email Format" });
-    return;
-  }
-
+const deleteUserHandler = async (req, res) => {
   try {
-    const results = await new Promise((resolve, reject) => {
+    const userId = req.params.id;
+
+    await new Promise((resolve, reject) => {
       con.query(
-        `SELECT * FROM users WHERE email= ?`,
-        [email],
+        "DELETE FROM users WHERE id = ?",
+        [userId],
         (error, results, fields) => {
           if (error) {
+            console.error("Database error:", error);
             reject(error);
           } else {
+            console.log("User deleted successfully");
             resolve(results);
           }
         }
       );
     });
 
-    if (results.length === 0) {
-      res.status(401).json({ error: "Invalid email or password" });
-      return;
-    }
-
-    const hashedPassword = results[0].password;
-
-    const passwordMatch = await bcrypt.compare(password, hashedPassword);
-
-    if (!passwordMatch) {
-      res.status(401).json({ error: "Invalid email or password" });
-    }
-
-    const token = jwt.sign(
-      {
-        userId: results[0].id,
-        email: results[0].email,
-      },
-      secretKey,
-      {
-        expiresIn: "1h",
-      }
-    );
-    res.status(200).json({ message: "Login successful", token });
+    res.status(200).json({ message: 'User deleted successfully' });
   } catch (error) {
-    res.status(500).json({ error: "Internal Server Error" });
+    console.error('Handler error:', error);
+    res.status(error.code || 500).json({ error: 'Internal Server Error' });
   }
 };
 
-module.exports = {
-  loginHandler,
-  registerHandler,
-};
+module.exports = { updateUserHandler, deleteUserHandler };
