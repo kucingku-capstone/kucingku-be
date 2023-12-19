b1a86ec69f98f78a4c80efdac34e8586902d5050
Akbar Ihsanul Ahadin <114788608+xitsaix@users.noreply.github.com>
2023-12-19 21:44:05 +0700

    Update index.js for NoSQL db

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
-const express = require("express");
-const app = express();
-const authRoute = require('./routes/auth')
-const indexRoute = require('./routes/index');
-const bodyParser = require("body-parser");
-import FileUpload from "express-fileupload";
-import cors from "cors";
-import CatRoute from "./routes/CatRoute.js";
-import UserInterestRoute from "./routes/userInterestRoute.js";
+import express from 'express';
+import FileUpload from 'express-fileupload';
+import cors from 'cors';
+import CatRoute from './routes/CatRoute.js';
+import UserInterestRoute from './routes/userInterestRoute.js';
 
+const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(FileUpload());
-app.use(express.static("public"));
-app.use(authRoute, indexRoute, CatRoute, UserInterestRoute);
+app.use(express.static('public'));
+app.use(CatRoute, UserInterestRoute);
 
-app.listen(port, (req, res) => {
+app.listen(port, () => {
     console.log(`listen to port: ${port}`);
 });
 
 process.on('unhandledRejection', (err) => {
     console.log(err);
     process.exit();
-})
+});
