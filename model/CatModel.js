0034ab0a3c47c425789445c5b6652b430033fbb2
Akbar Ihsanul Ahadin <114788608+xitsaix@users.noreply.github.com>
2023-12-20 12:31:26 +0700

    Update CatModel.js for Firestore

--- model/CatModel.js
+++ model/CatModel.js
@@ -1,22 +1,5 @@
-import mongoose from 'mongoose';
+import firestore from '../utils/db.js';
 
-mongoose.connect('env.config', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,   
-});
-
-const catSchema = new mongoose.Schema({
-    name: String,
-    age: String,
-    gender: String,
-    size: String,
-    coat: String,
-    breed: String,
-    description: String,
-    image: String,
-    url: String,
-});
-
-const Cat = mongoose.model('ShelterCat', catSchema);
+const Cat = firestore.collection('shelterCats');
 
 export default Cat;
