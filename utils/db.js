37297f73d6241fc2e7c3f855996387e8ed13b9dc
Akbar Ihsanul Ahadin <114788608+xitsaix@users.noreply.github.com>
2023-12-22 00:56:01 +0700

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,13 +1,13 @@
 import { Firestore } from '@google-cloud/firestore';
 
 const firestore = new Firestore({
-    projectId: '...',  //Fill Project Id from Firestore
-    keyFilename: "..."  //Fill path/to/the/serviceAccountKey.json
+    projectId: 'capstone-kucingku',  //Fill Project Id from Firestore
+    keyFilename: "nothing.json"  //Fill path/to/the/serviceAccountKey.json
 });
 
 var admin = require("firebase-admin");
 
-var serviceAccount = require("...");  //Fill path/to/the/serviceAccountKey.json
+var serviceAccount = require("nothing.json");  //Fill path/to/the/serviceAccountKey.json
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
